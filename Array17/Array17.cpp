103bf1a7aba6621a9d0162db932661bd3507bdb9
Good <Good@GOOD->
2021-12-01 16:52:54 +0300

    Добавьте файлы проекта.

---
+++ Array17/Array17.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int length = 5;
+int mass[5]{ 0,1,2,3,4 };
+
+void getCoinciding();
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	getCoinciding();
+}
+
+void getCoinciding()
+{
+	int coincNum = 0;
+	int coincNumTemp = 0;
+	for (int i = 0; i < length; i++)
+	{
+		if (mass[i] == i)
+		{
+			coincNum = mass[i];
+			coincNumTemp += coincNum;
+		}
+	}
+	cout << endl << "Сумма чисел: " << coincNumTemp << endl;
+}
